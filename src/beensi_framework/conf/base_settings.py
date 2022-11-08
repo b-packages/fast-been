2164176c838a5cb24daf412210026c28c37105a9
navid <navidsoleymani@ymail.com>
2022-11-08 22:15:21 +0330

    create BaseSettings

---
+++ src/beensi_framework/conf/base_settings.py
@@ -0,0 +1,28 @@
+from functools import lru_cache
+
+from pydantic import (
+    BaseSettings as BSettings,
+    BaseModel as BModel,
+)
+
+
+class _JWTConf(BModel):
+    SECRET_KEY: str = 'SECRET_KEY'
+    ALGORITHM: str = 'HS256'
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
+
+
+class BaseSettings(BSettings):
+    SECRET_KEY: str = 'SECRET_KEY'
+    SQLALCHEMY_DATABASE_URL: str = 'sqlite:///./db.sqlite3'
+    DEBUG: bool = True
+    JWT_CONF: _JWTConf = _JWTConf()
+
+    class Config:
+        env_file = '.env'
+        env_file_encoding = 'utf-8'
+
+
+@lru_cache()
+def get_settings():
+    return BaseSettings()
