ad0c18523e8b584eb734eb31d6d900c9918c8ffb
navid <navidsoleymani@ymail.com>
2022-11-09 23:24:32 +0330

    conf setup file

--- setup.py
+++ setup.py
@@ -2,17 +2,19 @@ from setuptools import setup, find_packages
 
 setup(
     name='beensi_framework',
-    version='0.01',
+    version='0.1',
     license='BEENSI',
     author='nvd',
     author_email='navidsoleymani@ymail.com',
     packages=find_packages('src'),
     package_dir={'': 'src'},
-    # url='',
-    # keywords='',
+    url='https://github.com/b-packages/beensi-framework.git',
+    keywords='beensi_framework',
     install_requires=[
         'fastapi',
         'pydantic',
         'SQLAlchemy',
+        'python-jose',
+        'passlib',
     ],
 )
