996f1059baf72417dd64479cd8cdd8cb7d1ba424
nvd <navidsoleymani@ymail.com>
2022-12-24 15:33:32 +0330

    add input and output controller

--- src/beensi_framework/api/cotrollers/__base.py
+++ src/beensi_framework/api/cotrollers/__base.py
@@ -29,11 +29,18 @@ class __Base:
 
     field_control_options: dict = None
 
-    def validate_data(self, **input_data):
-        data = {}
-        for k, v in input_data:
-            data[k] = self.__field_class(k, v)
-        return data
+    __valid_data: dict | None = None
+
+    @property
+    def valid_data(self):
+        return self.__valid_data
+
+    def validate_data(self, **data):
+        valid_data = {}
+        for k, v in data:
+            valid_data[k] = self.__field_class(k, v)
+        self.__valid_data = valid_data
+        return valid_data
 
     def __field_class(self, k, v):
         field = Fields[BASE]
@@ -57,3 +64,61 @@ class __Base:
             field = Fields[BASE]
             base_value_input.update(self.field_control_options[k])
             return field(**base_value_input)
+
+    output_field_list: list | None = None
+
+    @property
+    def output(self, many=False) -> dict | list | None:
+        if not self.valid_data:
+            return None
+        if not many:
+            return self.__output_many_false
+        return self.__output_many_true
+
+    @property
+    def __output_many_false(self) -> dict:
+        rst = {}
+        for f in self.valid_data:
+            if f.name in self.output_field_list:
+                if f.write_only is False:
+                    rst[f.name] = f.value
+        return rst
+
+    @property
+    def __output_many_true(self) -> list:
+        rst = []
+        for itm in self.valid_data:
+            tmp = {}
+            for f in itm:
+                if f.name in self.output_field_list:
+                    if f.write_only is False:
+                        tmp[f.name] = f.value
+            rst.append(tmp)
+        return rst
+
+    @property
+    def input(self, many=False) -> dict | list | None:
+        if not self.valid_data:
+            return None
+        if not many:
+            return self.__input_many_false
+        return self.__input_many_true
+
+    @property
+    def __input_many_false(self):
+        rst = {}
+        for f in self.valid_data:
+            if f.read_only is False:
+                rst[f.name] = f.value
+        return rst
+
+    @property
+    def __input_many_true(self):
+        rst = []
+        for itm in self.valid_data:
+            tmp = {}
+            for f in itm:
+                if f.read_only is False:
+                    tmp[f.name] = f.value
+            rst.append(tmp)
+        return rst
