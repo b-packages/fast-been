5feffbd008427dd69689e08af1218820400ef397
nvd <navidsoleymani@ymail.com>
2022-12-24 12:38:43 +0330

    create positivefloatfield shcema

---
+++ src/beensi_framework/utils/schemas/fields/__positive_float_field.py
@@ -0,0 +1,19 @@
+from pydantic import root_validator
+
+from . import FloatField
+from beensi_framework.utils.macros import POSITIVE_FLOAT, MIN_VALUE
+
+
+class PositiveFloatField(FloatField):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.min_value = max(0, kwargs[MIN_VALUE] if MIN_VALUE in kwargs else 0)
+        self.TYPE = POSITIVE_FLOAT
+
+    @root_validator
+    def check_type(cls, values):
+        values = super().check_type(values)
+        if values['value']:
+            if values['value'] < 0:
+                raise Exception(f'Type \"{values["name"]}\" should be \"{values["TYPE"]}\".')
+        return values
