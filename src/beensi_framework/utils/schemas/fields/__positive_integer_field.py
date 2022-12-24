58f73322f408f474c40f04ce069e6dda81694067
nvd <navidsoleymani@ymail.com>
2022-12-24 12:36:15 +0330

    create positiveintegerfield shcema

---
+++ src/beensi_framework/utils/schemas/fields/__positive_integer_field.py
@@ -0,0 +1,19 @@
+from pydantic import root_validator
+
+from . import IntegerField
+from beensi_framework.utils.macros import MIN_VALUE, POSITIVE_INTEGER
+
+
+class PositiveIntegerField(IntegerField):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.min_value = max(0, kwargs[MIN_VALUE] if MIN_VALUE in kwargs else 0)
+        self.TYPE = POSITIVE_INTEGER
+
+    @root_validator
+    def check_type(cls, values):
+        values = super().check_type(values)
+        if values['value']:
+            if values['value'] < 0:
+                raise Exception(f'Type \"{values["name"]}\" should be \"{values["TYPE"]}\".')
+        return values
