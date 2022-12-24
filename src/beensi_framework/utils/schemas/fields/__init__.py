852558faca2eb9390517bc94f5783a250d54fea1
nvd <navidsoleymani@ymail.com>
2022-12-24 14:50:37 +0330

    add fields to __init__

--- src/beensi_framework/utils/schemas/fields/__init__.py
+++ src/beensi_framework/utils/schemas/fields/__init__.py
@@ -7,3 +7,6 @@ from .__float_field import FloatField
 from .__positive_float_field import PositiveFloatField
 from .__negative_float_field import NegativeFloatField
 from .__string_field import StringField
+
+# ...
+from .__fields import Fields
