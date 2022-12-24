dbc1fa88b1b7d75a9a561965b2de33b3d4e4d3cf
nvd <navidsoleymani@ymail.com>
2022-12-24 15:42:02 +0330

    create Getter class for get data from db

---
+++ src/beensi_framework/api/cotrollers/__geter.py
@@ -0,0 +1,14 @@
+from .__base import __Base
+
+
+class Getter(__Base):
+
+    def run(self, **kwargs):
+        tmp = self.__get(**kwargs)
+        if tmp is None:
+            return None
+        self.validate_data(**tmp.to_dict())
+        return self.output
+
+    def __get(self, **kwargs):
+        return self.queryset.filter(**kwargs).first()
