c7e01316c25cb8b23ecd26ab1c79072088d51e23
nvd <navidsoleymani@ymail.com>
2022-12-24 14:55:25 +0330

    create validate data by field_control_option field

--- src/beensi_framework/api/cotrollers/__base.py
+++ src/beensi_framework/api/cotrollers/__base.py
@@ -1,3 +1,7 @@
+from beensi_framework.utils.schemas.fields import Fields
+from beensi_framework.utils.macros import TYPE, BASE
+
+
 class __Base:
     def __init__(self, db, model):
         self.__db = db
@@ -22,3 +26,34 @@ class __Base:
         if self.__queryset is None:
             self.__queryset = self.db.query(self.model).filter(is_active=True).all()
         return self.__queryset
+
+    field_control_options: dict = None
+
+    def validate_data(self, **input_data):
+        data = {}
+        for k, v in input_data:
+            data[k] = self.__field_class(k, v)
+        return data
+
+    def __field_class(self, k, v):
+        field = Fields[BASE]
+        base_value_input = {
+            'name': k,
+            'value': v,
+            'queryset': self.queryset,
+        }
+        if not self.field_control_options:
+            return field(**base_value_input)
+        if not self.field_control_options[k]:
+            return field(**base_value_input)
+        if not self.field_control_options[k][TYPE]:
+            base_value_input.update(self.field_control_options[k])
+            return field(**base_value_input)
+        try:
+            field = Fields[self.field_control_options[k][TYPE]]
+            base_value_input.update(self.field_control_options[k])
+            return field(**base_value_input)
+        except:
+            field = Fields[BASE]
+            base_value_input.update(self.field_control_options[k])
+            return field(**base_value_input)
