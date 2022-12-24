57cd227477ffb94753967ab2b2658b5a41257fea
nvd <navidsoleymani@ymail.com>
2022-12-24 07:09:36 +0330

    change raise exception error

--- src/beensi_framework/utils/validators/regex/_checker.py
+++ src/beensi_framework/utils/validators/regex/_checker.py
@@ -1,9 +1,6 @@
 import re
 
-from fastapi import HTTPException
-
 
 def regex_checker(regex_pattern, string: str, error_messages: list):
     if not re.match(regex_pattern, string):
-        raise HTTPException(
-            status_code=400, detail={'messages': error_messages})
+        raise Exception(f'{error_messages}')
