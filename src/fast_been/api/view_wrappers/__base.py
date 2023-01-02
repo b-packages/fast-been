a5dbb0242bc88e443aa29f02bf76c27bb25544f9
nvd <navid_soleymani@ymail.com>
2023-01-02 05:16:00 -0800

    restruct response

--- src/fast_been/api/view_wrappers/__base.py
+++ src/fast_been/api/view_wrappers/__base.py
@@ -5,6 +5,7 @@ from fastapi import Response, Request, HTTPException
 from fast_been.conf.base_settings import BASE_SETTINGS
 from fast_been.utils.date_time import now
 from fast_been.utils.generators.auth.jwt import access_token
+from fast_been.utils.schemas.http import Response as FastBeenResponse
 
 JWT_ALGORITHM = BASE_SETTINGS.JWT.ALGORITHM
 JWT_SECRET_KEY = BASE_SETTINGS.JWT.SECRET_KEY
@@ -20,7 +21,7 @@ class Base:
     __controller_class = None
     __methods = None
     __is_login: bool = False
-    __response: Response = None
+    __response: FastBeenResponse = FastBeenResponse()
 
     def __init__(self, controller_class, methods=None, is_login=False):
         self.__controller_class = controller_class
@@ -30,7 +31,7 @@ class Base:
     def run(self, request: Request, lookup_field=None, input_data=None):
         try:
             if not self.__is_login_check(request):
-                return self.response
+                return self.http_response
             func = self.func_method(request.method.lower())
             func(
                 input_data=input_data,
@@ -41,7 +42,7 @@ class Base:
                 ordering=request.query_params.get('ordering'),
                 search=request.query_params.get('search')
             )
-            return self.response
+            return self.http_response
         except HTTPException as exp:
             return exp
 
@@ -50,43 +51,43 @@ class Base:
             lookup_field=lookup_field, page=page, page_size=page_size,
             filters=filters, ordering=ordering, search=search)
         if output:
-            self.response.status_code = 200
-            self.response.body = self.response.render(output)
+            self.__response.status_code = 200
+            self.__response.content = output
         else:
-            self.response.status_code = 404
+            self.__response.status_code = 404
 
     def post(self, input_data: dict, **kwargs):
         output = self.controller.run(data=input_data)
-        self.response.status_code = 201
-        self.response.body = self.response.render(output)
+        self.__response.status_code = 201
+        self.__response.content = output
 
     def put(self, lookup_field, input_data, **kwargs):
         output = self.controller.run(
             lookup_field=lookup_field, data=input_data)
         if output:
-            self.response.status_code = 200
-            self.response.body = self.response.render(output)
+            self.__response.status_code = 200
+            self.__response.content = output
         else:
-            self.response.status_code = 404
+            self.__response.status_code = 404
 
     def patch(self, lookup_field, input_data, **kwargs):
         output = self.controller.run(
             lookup_field=lookup_field, data=input_data)
         if output:
-            self.response.status_code = 200
-            self.response.body = self.response.render(output)
+            self.__response.status_code = 200
+            self.__response.content = output
         else:
-            self.response.status_code = 404
+            self.__response.status_code = 404
 
     def delete(self, lookup_field, **kwargs):
         output = self.controller.run(lookup_field=lookup_field)
         if output:
-            self.response.status_code = 204
+            self.__response.status_code = 204
         else:
-            self.response.status_code = 404
+            self.__response.status_code = 404
 
     def __method_not_allowed(self, **kwargs):
-        return self.response(status_code=405)
+        self.__response.status_code = 405
 
     def func_method(self, method):
         if method not in self.methods:
@@ -108,10 +109,17 @@ class Base:
         return self.__methods
 
     @property
-    def response(self):
-        if self.__response is None:
-            self.__response = Response()
-        return self.__response
+    def http_response(self):
+        rslt = Response(
+            status_code=self.__response.status_code,
+            content=self.__response.content,
+            headers=self.__response.headers,
+            media_type=self.__response.media_type,
+            background=self.__response.background,
+        )
+        for c in self.__response.set_cookies:
+            rslt.set_cookie(**c.dict())
+        return
 
     @property
     def controller(self):
@@ -130,10 +138,12 @@ class Base:
         if datetime.utcfromtimestamp(token_decoded['exp']) < now():
             return False
         request.scope['beanser_pid'] = token_decoded['sub']
-        self.response.set_cookie(
-            key='Authorization',
-            value=access_token(pid=token_decoded['sub']),
-            secure=False,
-            httponly=True,
+        self.__response.set_cookies.append(
+            **{
+                'key': 'Authorization',
+                'value': access_token(pid=token_decoded['sub']),
+                'secure': False,
+                'httponly': True,
+            }
         )
         return True
