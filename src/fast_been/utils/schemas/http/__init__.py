2d9002eb1714c271a9ba7eba989af977bba9de6a
nvd <navid_soleymani@ymail.com>
2023-01-02 05:15:34 -0800

    create response

---
+++ src/fast_been/utils/schemas/http/__init__.py
@@ -0,0 +1,1 @@
+from .__response import Response
---
+++ src/fast_been/utils/schemas/http/__response.py
@@ -0,0 +1,22 @@
+from pydantic import BaseModel
+import typing
+
+
+class SetCookie(BaseModel):
+    key: str
+    value: str = ''
+    max_age: typing.Optional[int] = None
+    expires: typing.Optional[int] = None
+    path: str = '/'
+    domain: typing.Optional[str] = None
+    secure: bool = False
+    httponly: bool = False
+
+
+class Response(BaseModel):
+    content: typing.Any = None
+    status_code: int = 200
+    headers: typing.Optional[typing.Mapping[str, str]] = None
+    media_type: typing.Optional[str] = None
+    background: typing.Any = None
+    set_cookies: typing.List[SetCookie]
