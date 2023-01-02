bad7a44a156c469d852077ce7ea0619da0f0f542
nvd <navid_soleymani@ymail.com>
2023-01-02 11:59:49 -0800

    delete extra

--- src/fast_been/utils/validators/regex/patterns/__beanser_name.py
+++
@@ -1,1 +0,0 @@
-BEANSER_NAME = r'^[a-zA-Z][a-zA-Z0-9-_]{2,127}$'
--- src/fast_been/utils/validators/regex/patterns/__init__.py
+++
@@ -1,3 +0,0 @@
-from .__beanser_name import BEANSER_NAME
-from .__email_address import EMAIL_ADDRESS
-from .__password import PASSWORD
