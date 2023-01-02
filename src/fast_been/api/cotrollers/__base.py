3c8d4615a7361533564ad7a3e78cd7465c752b45
nvd <navid_soleymani@ymail.com>
2023-01-02 14:44:17 -0800

    refactor

--- src/fast_been/api/cotrollers/__base.py
+++ src/fast_been/api/cotrollers/__base.py
@@ -14,20 +14,39 @@ from fast_been.utils.exceptions.http import (
     MaximumLengthInputValueHTTPException,
     MinimumLengthInputValueHTTPException,
 )
-from fast_been.utils.macros import ALL
+
+from .__macro import *
 
 
 class Base(ABC):
     model = None
     db: Session = None
-    lookup_field_name: str = 'pid'
-    field_control_options: Union[dict, None] = None
+    lookup_field_name: str = PID
     input_fields: Union[list, str, None] = None
     output_fields: Union[list, str, None] = None
 
     @abstractmethod
     def run(self, *args, **kwargs):
         pass
+
+    __field_control_options: Union[dict, None] = None
+
+    @property
+    def field_control_options(self):
+        if self.__field_control_options:
+            return self.__field_control_options
+        return {}
+
+    @staticmethod
+    def __field_control_options_sort_key_func(item):
+        return FIELD_CONTROL_OPTIONS_METER[item[0]]
+
+    @field_control_options.setter
+    def field_control_options(self, value: dict):
+        tmp = {}
+        for k, v in value.items():
+            tmp[k] = dict(sorted(v.items(), key=self.__field_control_options_sort_key_func))
+        self.__field_control_options = tmp
 
     __queryset_ = None
 
@@ -38,143 +57,92 @@ class Base(ABC):
         return self.__queryset_
 
     def input_data(self, **kwargs):
-        if type(self.input_fields) == list:
-            return self.__input_for_input_fields(**kwargs)
-        if self.input_fields == ALL:
-            return self.__input_for_all_fields(**kwargs)
-        return {}
-
-    def __input_for_input_fields(self, **kwargs):
         rslt = {}
         for key, value in kwargs.items():
             if key in self.input_fields:
                 if key in self.field_control_options:
                     for i in self.field_control_options[key]:
-                        if i == 'converter':
-                            continue
-                        value = self.__field_control_options_mapper_input(controller_name=i, key=key, value=value)
-                    if 'converter' in self.field_control_options[key]:
-                        value = self.__field_control_options_mapper_input(controller_name='converter', key=key,
-                                                                          value=value)
+                        value = self.__field_control_options_mapper(controller_name=i, key=key, value=value)
                 if value:
                     rslt[key] = value
-        return rslt
-
-    def __input_for_all_fields(self, **kwargs):
-        rslt = {}
-        for key, value in kwargs.items():
-            if key in self.field_control_options:
-                for i in self.field_control_options[key]:
-                    if i == 'converter':
-                        continue
-                    value = self.__field_control_options_mapper_input(controller_name=i, key=key, value=value)
-                if 'converter' in self.field_control_options[key]:
-                    value = self.__field_control_options_mapper_input(controller_name='converter', key=key, value=value)
-            if value:
-                rslt[key] = value
         return rslt
 
     def output_data(self, **kwargs):
-        if type(self.output_fields) == list:
-            return self.__output_for_output_fields(**kwargs)
-        if self.output_fields == ALL:
-            return self.__output_for_all_fields(**kwargs)
-        return {}
-
-    def __output_for_output_fields(self, **kwargs):
         rslt = {}
         for key, value in kwargs.items():
-            if key in self.output_fields:
-                if key in self.field_control_options:
-                    for i in self.field_control_options[key]:
-                        value = self.__field_control_options_mapper_output(controller_name=i, key=key, value=value)
-                if value:
-                    rslt[key] = value
-        return rslt
-
-    def __output_for_all_fields(self, **kwargs):
-        rslt = {}
-        for key, value in kwargs.items():
-            if key in self.field_control_options:
-                for i in self.field_control_options[key]:
-                    value = self.__field_control_options_mapper_output(controller_name=i, key=key, value=value)
-            if value:
+            if key in self.output_fields and value:
                 rslt[key] = value
         return rslt
 
     def __default(self, key, value):
         if not value:
-            return self.field_control_options[key]['default']
+            return self.field_control_options[key][DEFAULT]
         return value
 
     def __required(self, key, value):
-        if self.field_control_options[key]['required'] and value is None:
+        if self.field_control_options[key][REQUIRED] and value is None:
             raise RequiredInputValueHTTPException(key)
         return value
 
     def __allow_null(self, key, value):
-        if not self.field_control_options[key]['allow_null'] and value is None:
+        if not self.field_control_options[key][ALLOW_NULL] and value is None:
             raise AllowNullInputValueHTTPException(key)
         return value
 
     def __allow_blank(self, key, value):
-        if not self.field_control_options[key]['allow_blank'] and self.__is_blank(value):
+        if not self.field_control_options[key][ALLOW_BLANK] and self.__is_blank(value):
             raise AllowBlankInputValueHTTPException(key)
         return value
 
-    def __read_only(self, key, value):
-        if self.field_control_options[key]['read_only']:
-            return None
-        return value
-
-    def __write_only(self, key, value):
-        if self.field_control_options[key]['write_only']:
-            return None
-        return value
-
     def __unique(self, key, value):
-        if self.field_control_options[key]['unique'] and self.retrieve_data(**{key: value}):
+        if self.field_control_options[key][UNIQUE] and self.retrieve_data(**{key: value}):
             raise UniqueInputValueHTTPException(key)
         return value
 
     def __regex_validator(self, key, value):
-        regex_validator = self.field_control_options[key]['regex_validator']
+        regex_validator = self.field_control_options[key][REGEX_VALIDATOR]
         if not regex_validator.validate(value):
             raise RegexValidatorInputValueHTTPException(key, regex_validator.info)
         return value
 
     def __type(self, key, value):
-        type_input_value = self.field_control_options[key]['type']
-        if not self.__types__controller[type_input_value](value):
-            raise TypeInputValueHTTPException(key, type_input_value)
-        return value
+        type_input_value = self.field_control_options[key][TYPE]
+        if type(type_input_value) == str:
+            if not self.__types__controller[type_input_value](value):
+                raise TypeInputValueHTTPException(key, type_input_value)
+            return value
+        if type(type_input_value) in [tuple, list]:
+            for t in type_input_value:
+                if self.__types__controller[t](value):
+                    return value
+        raise TypeInputValueHTTPException(key, str(type_input_value))
 
     def __maximum_value(self, key, value):
-        maximum_value = self.field_control_options[key]['maximum_value']
+        maximum_value = self.field_control_options[key][MAXIMUM_VALUE]
         if not (type(value) in [int, float] and value <= maximum_value):
             raise MaximumValueInputValueHTTPException(key, maximum_value)
         return value
 
     def __minimum_value(self, key, value):
-        minimum_value = self.field_control_options[key]['minimum_value']
+        minimum_value = self.field_control_options[key][MINIMUM_VALUE]
         if not (type(value) in [int, float] and minimum_value <= value):
             raise MinimumValueInputValueHTTPException(key, minimum_value)
         return value
 
     def __maximum_length(self, key, value):
-        maximum_length = self.field_control_options[key]['maximum_length']
+        maximum_length = self.field_control_options[key][MAXIMUM_LENGTH]
         if not (type(value) in [str, list, dict, tuple, set] and len(value) <= maximum_length):
             raise MaximumLengthInputValueHTTPException(key, maximum_length)
         return value
 
     def __minimum_length(self, key, value):
-        minimum_length = self.field_control_options[key]['minimum_length']
+        minimum_length = self.field_control_options[key][MINIMUM_LENGTH]
         if not (type(value) in [str, list, dict, tuple, set] and minimum_length <= len(value)):
             raise MinimumLengthInputValueHTTPException(key, minimum_length)
         return value
 
     def __converter(self, key, value):
-        converter = self.field_control_options[key]['converter']
+        converter = self.field_control_options[key][CONVERTER]
         return converter(value)
 
     @staticmethod
@@ -193,19 +161,19 @@ class Base(ABC):
         if self.__types__controller_:
             return self.__types__controller_
         self.__types__controller_ = {
-            'numeric': self.__is_numeric,
-            'integer': self.__is_integer,
-            'positive_integer': self.__is_positive_integer,
-            'negative_integer': self.__is_negative_integer,
-            'float': self.__is_float,
-            'positive_float': self.__is_positive_float,
-            'negative_float': self.__is_negative_float,
-            'string': self.__is_string,
-            'list': self.__is_list,
-            'dictionary': self.__is_dictionary,
-            'tuple': self.__is_tuple,
-            'set': self.__is_set,
-            'boolean': self.__is_boolean,
+            NUMERIC: self.__is_numeric,
+            INTEGER: self.__is_integer,
+            POSITIVE_INTEGER: self.__is_positive_integer,
+            NEGATIVE_INTEGER: self.__is_negative_integer,
+            FLOAT: self.__is_float,
+            POSITIVE_FLOAT: self.__is_positive_float,
+            NEGATIVE_FLOAT: self.__is_negative_float,
+            STRING: self.__is_string,
+            LIST: self.__is_list,
+            DICTIONARY: self.__is_dictionary,
+            TUPLE: self.__is_tuple,
+            SET: self.__is_set,
+            BOOLEAN: self.__is_boolean,
         }
         return self.__types__controller_
 
@@ -268,43 +236,28 @@ class Base(ABC):
         if self.__controllers_:
             return self.__controllers_
         self.__controllers_ = {
-            'default': self.__default,
-            'required': self.__required,
-            'allow_null': self.__allow_null,
-            'allow_blank': self.__allow_blank,
-            'read_only': self.__read_only,
-            'write_only': self.__write_only,
-            'unique': self.__unique,
-            'regex_validator': self.__regex_validator,
-            'type': self.__type,
-            'maximum_value': self.__maximum_value,
-            'minimum_value': self.__minimum_value,
-            'maximum_length': self.__maximum_length,
-            'minimum_length': self.__minimum_length,
-            'converter': self.__converter,
+            DEFAULT: self.__default,
+            REQUIRED: self.__required,
+            ALLOW_NULL: self.__allow_null,
+            ALLOW_BLANK: self.__allow_blank,
+            TYPE: self.__type,
+            UNIQUE: self.__unique,
+            MAXIMUM_VALUE: self.__maximum_value,
+            MINIMUM_VALUE: self.__minimum_value,
+            MAXIMUM_LENGTH: self.__maximum_length,
+            MINIMUM_LENGTH: self.__minimum_length,
+            REGEX_VALIDATOR: self.__regex_validator,
+            CONVERTER: self.__converter,
         }
         return self.__controllers_
 
-    def __field_control_options_mapper_input(self, controller_name, key, value):
-        if controller_name in ['write_only', ]:
-            return value
-        return self.__field_control_options_mapper(
-            controller_name=controller_name, key=key, value=value)
-
-    def __field_control_options_mapper_output(self, controller_name, key, value):
-        if controller_name in ['write_only', ]:
-            return self.__field_control_options_mapper(
-                controller_name=controller_name, key=key, value=value)
-        return value
-
     def __field_control_options_mapper(self, controller_name, key, value):
-        if controller_name in self.__controllers:
-            return self.__controllers[controller_name](key=key, value=value)
-        return value
+        return self.__controllers[controller_name](
+            key=key, value=value) if controller_name in self.__controllers else value
 
     def create_data(self, **kwargs):
         inst = self.model(**kwargs)
-        inst.__set_pid(kwargs['pid'] if 'pid' in kwargs else None)
+        inst.__set_pid(kwargs[PID] if PID in kwargs else None)
         inst.__set_create_datetime()
         inst.__set_is_active()
         inst.__set_hashed()
@@ -328,8 +281,8 @@ class Base(ABC):
 
     def list_data(self, **kwargs):
         rslt = self.__queryset
-        if 'filters' in kwargs:
-            rslt = rslt.filter_by(**kwargs['filters']).all()
-        if 'ordering' in kwargs:
-            rslt = rslt.order_by(*kwargs['ordering']).all()
+        if FILTERS in kwargs:
+            rslt = rslt.filter_by(**kwargs[FILTERS]).all()
+        if ORDERING in kwargs:
+            rslt = rslt.order_by(*kwargs[ORDERING]).all()
         return rslt
