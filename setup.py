486a842ff97aa7d955d302ecad5f864c57229df6
nvd <navid_soleymani@ymail.com>
2023-01-13 02:28:23 -0800

    revert

--- setup.py
+++ setup.py
@@ -1,29 +1,20 @@
 from setuptools import setup, find_packages
 
-__version__ = '1.00'
-__name__ = 'fast_been'
-__author__ = 'hydra'
-__author_email__ = 'navidsoleymani@ymail.com'
-__license__ = 'BEENSI'
-__url__ = 'https://github.com/b-packages/fast-been.git'
-__keywords__ = 'fast_been fastapi',
-
 setup(
-    name=__name__,
-    version=__version__,
-    license=__license__,
-    author=__author__,
-    author_email=__author_email__,
+    name='fast_been',
+    version='1.00',
+    license='BEENSI',
+    author='nvd',
+    author_email='navidsoleymani@ymail.com',
     packages=find_packages('src'),
     package_dir={'': 'src'},
-    url=__url__,
-    keywords=__keywords__,
-    install_requires=[],
-    # install_requires=[
-    #     'fastapi',
-    #     'pydantic',
-    #     'SQLAlchemy',
-    #     'python-jose',
-    #     'passlib',
-    # ],
+    url='https://github.com/b-packages/fast-been.git',
+    keywords='fast_been fastapi',
+    install_requires=[
+        'fastapi',
+        'pydantic',
+        'SQLAlchemy',
+        'python-jose',
+        'passlib',
+    ],
 )
