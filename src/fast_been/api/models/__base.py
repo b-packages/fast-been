9ef783d0846749cf3e04fbcfe3621ba7a6fbd146
nvd <navid_soleymani@ymail.com>
2023-01-27 05:43:40 -0800

    redesigned

--- src/fast_been/api/models/__base.py
+++ src/fast_been/api/models/__base.py
@@ -1,45 +1,19 @@
-from fast_been.utils.json import JSON
+from sqlalchemy import Integer, Column, String, DateTime, ForeignKey, Text
 
-from sqlalchemy import Integer, Boolean, Column, String, DateTime, TEXT
-
-from fast_been.utils.generators.db.id import unique_id
 from fast_been.utils.hahsings import hash_row_db
-from fast_been.utils.date_time import now
+from fast_been.utils.json import JSON
 
 
 class Base(object):
     id = Column(Integer, primary_key=True, index=True)
-    pid = Column(String(255), index=True)
-    create_datetime = Column(DateTime())
-    hashed = Column(TEXT())
-    is_active = Column(Boolean())
-
-    def __set_pid(self, value=None):
-        if value:
-            self.pid = value
-        else:
-            self.pid = unique_id()
-
-    def __set_create_datetime(self):
-        self.create_datetime = now()
-
-    def __set_hashed(self):
-        self.hashed = self.hash()
-
-    def __set_is_active(self):
-        self.is_active = True
-
-    def __all_to_dict(self):
-        tmp = dict()
-        for column in self.__table__.columns:
-            tmp[column.name] = getattr(self, column.name)
-        return tmp
+    previous_state = Column(Integer, ForeignKey(id), primary_key=True)
+    pid = Column(String, index=True, nullable=False)
+    created_datetime = Column(DateTime, nullable=False)
+    hashed = Column(Text, nullable=True)
 
     def to_dict(self) -> dict:
         tmp = self.__all_to_dict().copy()
         tmp.pop('id')
-        tmp.pop('create_datetime')
-        tmp.pop('is_active')
         tmp.pop('hashed')
         return tmp
 
@@ -49,10 +23,12 @@ class Base(object):
         return tmp
 
     def hash(self) -> str:
-        tmp = self.__all_to_dict()
-        tmp.pop('id')
-        tmp.pop('is_active')
-        tmp.pop('hashed')
-        tmp = JSON.dumps(tmp)
+        tmp = self.to_json()
         tmp = hash_row_db(tmp)
+        return tmp
+
+    def __all_to_dict(self):
+        tmp = dict()
+        for column in self.__table__.columns:
+            tmp[column.name] = getattr(self, column.name)
         return tmp
