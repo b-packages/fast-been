06ce8de9cf7f7ccc855435e72cabe3cc5d3cd64e
nvd <navid_soleymani@ymail.com>
2023-01-28 02:18:27 -0800

    clean

--- src/fast_been/utils/schemas/controller/list/__output.py
+++ src/fast_been/utils/schemas/controller/list/__output.py
@@ -1,26 +1,33 @@
 import math
 from typing import Optional
-from pydantic import BaseModel
+from pydantic import BaseModel, HttpUrl
+
+COUNT = 'count'
+PAGE_SIZE = 'page_size'
+PAGE_NUMBER = 'page_number'
+NEXT_PAGE = 'next_page'
+PREVIOUS_PAGE = 'previous_page'
+URL = 'url'
 
 
 class OutputList(BaseModel):
     def __init__(self, **kwargs):
         nxt = None
-        number_of_pages = math.ceil(kwargs['count'] / kwargs['page_size'])
-        if kwargs['page_number'] < number_of_pages:
-            nxt = '?page={}&page_size={}'.format(kwargs['page_number'] + 1, kwargs['page_size'])
-        kwargs['next_page'] = nxt
+        number_of_pages = math.ceil(kwargs[COUNT] / kwargs[PAGE_SIZE])
+        if kwargs[PAGE_NUMBER] < number_of_pages:
+            nxt = '{}?page={}&page_size={}'.format(kwargs[URL], kwargs[PAGE_NUMBER] + 1, kwargs[PAGE_SIZE])
+        kwargs[NEXT_PAGE] = nxt
 
         prv = None
-        if 1 < kwargs['page_number']:
-            prv = '?page={}&page_size={}'.format(kwargs['page_number'] - 1, kwargs['page_size'])
-        kwargs['previous_page'] = prv
+        if 1 < kwargs[PAGE_NUMBER]:
+            prv = '{}?page={}&page_size={}'.format(kwargs[URL], kwargs[PAGE_NUMBER] - 1, kwargs[PAGE_SIZE])
+        kwargs[PREVIOUS_PAGE] = prv
 
         super(OutputList, self).__init__(**kwargs)
 
     count: int
     page_number: int = 1
     page_size: int
+    next_page: Optional[HttpUrl]
+    previous_page: Optional[HttpUrl]
     result: list = []
-    next_page: Optional[str]
-    previous_page: Optional[str]
