64e17f64eec91e544043906a25f057f2b50c3b66
nvd <navid_soleymani@ymail.com>
2023-01-27 18:50:52 -0800

    exception handling

--- src/fast_been/api/cotrollers/__updater.py
+++ src/fast_been/api/cotrollers/__updater.py
@@ -1,4 +1,9 @@
 from fast_been.utils.macros import ControllerType
+from fast_been.utils.exceptions.http import (
+    LookupFieldIsNotSetHTTPException,
+    ThereIsNoInputDataToRegisterHTTPException,
+    NotFoundHTTPException,
+)
 
 from . import Base
 
@@ -6,16 +11,18 @@ from . import Base
 class Updater(Base):
     def run(self, **kwargs):
         lookup_field = kwargs.get('lookup_field')
+        if lookup_field is None:
+            raise LookupFieldIsNotSetHTTPException()
         input_data = kwargs.get('input_data')
-        if lookup_field is None or input_data is None:
-            return None
+        if input_data is None:
+            raise ThereIsNoInputDataToRegisterHTTPException()
         return self.update(lookup_field, input_data)
 
     def update(self, lookup_field, input_data):
         input_ = self.input_data(**input_data)
         obj_ = self.update_data(lookup_field, **input_)
         if obj_ is None:
-            return None
+            raise NotFoundHTTPException()
         output_ = self.output_data(**obj_.to_dict())
         return output_
 
