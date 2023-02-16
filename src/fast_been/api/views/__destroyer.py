6ec8fcf1a9efb369ab009a7d2c559ef452ea2a09
nvd <navid_soleymani@ymail.com>
2023-02-15 20:34:19 -0800

    update

--- src/fast_been/api/views/__destroyer.py
+++ src/fast_been/api/views/__destroyer.py
@@ -1,5 +1,5 @@
 from fast_been.utils.http.response.status.code import NO_CONTENT
-from fastapi.responses import JSONResponse
+from fastapi.responses import Response
 
 from . import Base
 
@@ -8,13 +8,12 @@ class Destroyer(Base):
     expected_status_code = NO_CONTENT
 
     def get_response(self):
-        # rslt = JSONResponse(
-        #     status_code=self.response.status_code,
-        #     content=self.response.content,
-        #     headers=self.response.headers,
-        #     media_type=self.response.media_type,
-        #     background=self.response.background,
-        # )
-        # for c in self.response.cookies:
-        #     rslt.set_cookie(**c.dict())
+        rslt = Response(
+            status_code=self.response.status_code,
+            headers=self.response.headers,
+            media_type=self.response.media_type,
+            background=self.response.background,
+        )
+        for c in self.response.cookies:
+            rslt.set_cookie(**c.dict())
         return None
