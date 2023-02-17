5a63c33565b6fd17f4650771c755d857f07d269d
nvd <navid_soleymani@ymail.com>
2023-02-17 08:35:07 -0800

    restruct

--- src/fast_been/api/cotrollers/__destroyer.py
+++ src/fast_been/api/cotrollers/__destroyer.py
@@ -1,15 +1,21 @@
 from fast_been.utils.macros import ControllerType
-from fast_been.utils.exceptions.http import LookupFieldIsNotSetHTTPException, NotFoundHTTPException
+from fast_been.utils.exceptions.http import (
+    LookupFieldIsNotSetHTTPException,
+    NotFoundHTTPException,
+)
 
 from . import Base
 
 
 class Destroyer(Base):
-    def run(self, **kwargs):
-        lookup_field = kwargs.get('lookup_field')
-        if lookup_field is None:
+
+    def __init__(self, **kwargs):
+        self.__lookup_field = kwargs.get('lookup_field')
+        if self.__lookup_field is None:
             raise LookupFieldIsNotSetHTTPException()
-        return self.destroy(lookup_field)
+
+    def run(self):
+        return self.destroy(self.__lookup_field)
 
     def destroy(self, lookup_field):
         obj = self.destroy_data(lookup_field)
