493f762d930697781fea3b8bd7adc95de7c974a8
nvd <navid_soleymani@ymail.com>
2023-02-17 08:09:42 -0800

    restruct

--- src/fast_been/api/views/__destroyer.py
+++ src/fast_been/api/views/__destroyer.py
@@ -1,19 +1,7 @@
 from fast_been.utils.http.response.status.code import NO_CONTENT
-from fastapi.responses import Response
 
 from . import Base
 
 
 class Destroyer(Base):
     expected_status_code = NO_CONTENT
-
-    def get_response(self):
-        rslt = Response(
-            status_code=self.response.status_code,
-            headers=self.response.headers,
-            media_type=self.response.media_type,
-            background=self.response.background,
-        )
-        for c in self.response.cookies:
-            rslt.set_cookie(**c.dict())
-        return None
--- src/fast_been/api/views/__retriever.py
+++ src/fast_been/api/views/__retriever.py
@@ -1,7 +1,5 @@
-from fast_been.utils.http.response.status.code import OK
-
 from . import Base
 
 
 class Retriever(Base):
-    expected_status_code = OK
+    pass
