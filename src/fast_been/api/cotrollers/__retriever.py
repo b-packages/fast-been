af5a5b692facdecc84f99ce2ee692fcab7a08cb3
nvd <navid_soleymani@ymail.com>
2023-02-17 08:35:27 -0800

    restruct

--- src/fast_been/api/cotrollers/__retriever.py
+++ src/fast_been/api/cotrollers/__retriever.py
@@ -1,16 +1,21 @@
 from fast_been.utils.macros import ControllerType
-from fast_been.utils.exceptions.http import LookupFieldIsNotSetHTTPException, NotFoundHTTPException
+from fast_been.utils.exceptions.http import (
+    LookupFieldIsNotSetHTTPException,
+    NotFoundHTTPException,
+)
 
 from . import Base
 
 
 class Retriever(Base):
 
-    def run(self, **kwargs):
-        lookup_field = kwargs.get('lookup_field')
-        if lookup_field is None:
+    def __init__(self, **kwargs):
+        self.__lookup_field = kwargs.get('lookup_field')
+        if self.__lookup_field is None:
             raise LookupFieldIsNotSetHTTPException()
-        return self.retrieve(lookup_field)
+
+    def run(self):
+        return self.retrieve(self.__lookup_field)
 
     def retrieve(self, lookup_field):
         obj_ = self.retrieve_data(**{self.lookup_field_name: lookup_field})
