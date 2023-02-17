30965327dc386d6a25af516925cbf65bbba9c566
nvd <navid_soleymani@ymail.com>
2023-02-17 08:34:58 -0800

    restruct

--- src/fast_been/api/cotrollers/__creator.py
+++ src/fast_been/api/cotrollers/__creator.py
@@ -1,17 +1,21 @@
 from fast_been.utils.macros import ControllerType
-from fast_been.utils.exceptions.http import ThereIsNoInputDataToRegisterHTTPException
+from fast_been.utils.exceptions.http import (
+    ThereIsNoInputDataToRegisterHTTPException,
+)
 
 from . import Base
 
 
 class Creator(Base):
-    instance = None
 
-    def run(self, **kwargs):
-        input_data = kwargs.get('input_data')
-        if input_data is None:
+    def __init__(self, **kwargs):
+        self.__input_data = kwargs.get('input_data')
+        if self.__input_data is None:
             raise ThereIsNoInputDataToRegisterHTTPException()
-        return self.create(input_data)
+        self.instance = None
+
+    def run(self):
+        return self.create(self.__input_data)
 
     def create(self, input_data: dict):
         input_ = self.input_data(**input_data)
