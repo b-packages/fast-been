05ca8e96125f724b8c0048202fdf754bfab23d31
nvd <navid_soleymani@ymail.com>
2023-02-26 11:02:42 -0800

    fixed bugs

--- src/fast_been/utils/schemas/http/__request.py
+++ src/fast_been/utils/schemas/http/__request.py
@@ -12,8 +12,8 @@ class Request(BaseModel):
             qp = dict(base_request.query_params)
             kwargs[PAGE] = qp.pop(PAGE) if PAGE in qp else BASE_SETTINGS.PAGINATION.DEFAULT_START_PAGE_NUMBER
             kwargs[PAGE_SIZE] = qp.pop(PAGE_SIZE) if PAGE_SIZE in qp else BASE_SETTINGS.PAGINATION.PAGE_SIZE
-            kwargs[ORDERING] = qp.pop(ORDERING) if ORDERING in qp else BASE_SETTINGS.PAGINATION.PAGE_SIZE
-            kwargs[SEARCH] = qp.pop(SEARCH) if SEARCH in qp else BASE_SETTINGS.PAGINATION.PAGE_SIZE
+            kwargs[ORDERING] = qp.pop(ORDERING) if ORDERING in qp else BASE_SETTINGS.PAGINATION.ORDERING
+            kwargs[SEARCH] = qp.pop(SEARCH) if SEARCH in qp else None
             kwargs[FILTERS] = qp
             tmp = str(base_request.url)
             kwargs[URL] = tmp.split('?')[0]
