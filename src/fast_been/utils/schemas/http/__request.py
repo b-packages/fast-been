6198f2fc0aa1f9cbe49b25f11cc4314f55f23e39
nvd <navid_soleymani@ymail.com>
2023-02-26 10:45:21 -0800

    fixed bugs

--- src/fast_been/utils/schemas/http/__request.py
+++ src/fast_been/utils/schemas/http/__request.py
@@ -7,7 +7,17 @@ from .__macro import *
 class Request(BaseModel):
 
     def __init__(self, **kwargs):
-        super(Request, self).__init__(**self.__kwargs(**kwargs))
+        base_request = kwargs[BASE_REQUEST] = kwargs.get(REQUEST)
+        if base_request:
+            qp = dict(base_request.query_params)
+            kwargs[PAGE] = qp.pop(PAGE) if PAGE in qp else BASE_SETTINGS.PAGINATION.DEFAULT_START_PAGE_NUMBER
+            kwargs[PAGE_SIZE] = qp.pop(PAGE_SIZE) if PAGE_SIZE in qp else BASE_SETTINGS.PAGINATION.PAGE_SIZE
+            kwargs[ORDERING] = qp.pop(ORDERING) if ORDERING in qp else BASE_SETTINGS.PAGINATION.PAGE_SIZE
+            kwargs[SEARCH] = qp.pop(SEARCH) if SEARCH in qp else BASE_SETTINGS.PAGINATION.PAGE_SIZE
+            kwargs[FILTERS] = qp
+            tmp = str(base_request.url)
+            kwargs[URL] = tmp.split('?')[0]
+        super(Request, self).__init__(**kwargs)
 
     # input data
     input_data: Optional[dict]
@@ -24,21 +34,3 @@ class Request(BaseModel):
     search: Optional[list]
     # base request
     base_request: Any
-
-    @staticmethod
-    def __base_request_decoder(**kwargs):
-        base_request = kwargs[BASE_REQUEST] = kwargs.get(REQUEST)
-        if base_request:
-            qp = base_request.query_params.copy()
-            kwargs[PAGE] = qp.pop(PAGE) if PAGE in qp else BASE_SETTINGS.PAGINATION.DEFAULT_START_PAGE_NUMBER
-            kwargs[PAGE_SIZE] = qp.pop(PAGE_SIZE) if PAGE_SIZE in qp else BASE_SETTINGS.PAGINATION.PAGE_SIZE
-            kwargs[ORDERING] = qp.pop(ORDERING) if ORDERING in qp else BASE_SETTINGS.PAGINATION.PAGE_SIZE
-            kwargs[SEARCH] = qp.pop(SEARCH) if SEARCH in qp else BASE_SETTINGS.PAGINATION.PAGE_SIZE
-            kwargs[FILTERS] = FILTERS
-            tmp = str(base_request.url)
-            kwargs[URL] = tmp.split('?')[0]
-        return kwargs
-
-    def __kwargs(self, **kwargs):
-        kwargs = self.__base_request_decoder(**kwargs)
-        return kwargs
