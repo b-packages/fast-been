112d5d0b0348c8032bf550b847a4e7a6f0bcf4de
nvd <navid_soleymani@ymail.com>
2023-03-02 11:08:10 -0800

    change values

--- src/fast_been/conf/base_settings/__mail.py
+++ src/fast_been/conf/base_settings/__mail.py
@@ -5,6 +5,6 @@ class MailSetting(BModel):
     USERNAME: str
     PASSWORD: str
     FROM: str
-    PORT: int = 587
-    SERVER: str = 'smtp.gmail.com'
+    PORT: int
+    SERVER: str
     FROM_NAME: str
