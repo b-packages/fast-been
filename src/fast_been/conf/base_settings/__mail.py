6d7797f78025778608265711c1d78f4901b81bc7
nvd <navid_soleymani@ymail.com>
2023-03-02 11:05:23 -0800

    renaming

--- src/fast_been/conf/base_settings/__mail.py
+++ src/fast_been/conf/base_settings/__mail.py
@@ -2,9 +2,9 @@ from pydantic import BaseModel as BModel
 
 
 class MailSetting(BModel):
-    MAIL_USERNAME: str
-    MAIL_PASSWORD: str
-    MAIL_FROM: str
-    MAIL_PORT: int = 587
-    MAIL_SERVER: str = 'smtp.gmail.com'
-    MAIL_FROM_NAME: str
+    USERNAME: str
+    PASSWORD: str
+    FROM: str
+    PORT: int = 587
+    SERVER: str = 'smtp.gmail.com'
+    FROM_NAME: str
