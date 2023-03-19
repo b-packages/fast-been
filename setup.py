f91cace872044dadd79c143227cb12ed6f949ffb
nvd <navid_soleymani@ymail.com>
2023-03-19 00:19:54 -0700

    update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='fast_been',
-    version='1.06',
+    version='1.07',
     license='BEENSI',
     author='nvd',
     author_email='navidsoleymani@ymail.com',
