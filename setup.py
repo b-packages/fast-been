3a18dda18d862b362c20e045eb0d7b8726b1760f
navid soleymani <30310874+navidsoleymani@users.noreply.github.com>
2023-03-27 15:30:19 +0330

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='fast_been',
-    version='1.07',
+    version='1.08',
     license='BEENSI',
     author='nvd',
     author_email='navidsoleymani@ymail.com',
